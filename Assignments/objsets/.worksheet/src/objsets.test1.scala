16de5d0cc50ace9f1c4147b4dbd55d1682b7ee0c
Devesh <devesh.acharya@cba.com.au>
2016-01-14 10:12:24 +1100

    Added Objset

---
+++ Assignments/objsets/.worksheet/src/objsets.test1.scala
@@ -0,0 +1,6 @@
+package objsets
+
+object test1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(75); 
+  println("Welcome to the Scala worksheet")}
+ 
+}
---
+++ Assignments/objsets/src/main/scala/objsets/test1.sc
@@ -0,0 +1,6 @@
+package objsets
+
+object test1 {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+ 
+}
